e67af362ceaee1499e596c30979582f7297dae12
lutzzdias <thiagolutzdias@hotmail.com>
2022-11-06 17:31:27 -0400

    Implement getReviewById (untested)

--- controllers/review_controller.js
+++ controllers/review_controller.js
@@ -4,5 +4,6 @@ const service = require("../services/review_service");
 const router = Router();
 
 router.get("/", service.getReviews);
+router.get("/:id", service.getReviewById);
 
 module.exports = router;
--- services/review_service.js
+++ services/review_service.js
@@ -2,9 +2,18 @@ const { Review } = require("../models/");
 
 const getReviews = async (req, res) => {
   const reviews = await Review.findAll();
-  res.json(reviews);
+  res.status(200).json(reviews);
+};
+
+const getReviewById = async (req, res) => {
+  const id = req.params.id;
+  const review = await Review.findByPk(id);
+
+  if (review == null) return res.status(404).send("Review not found!");
+  else return res.status(200).json(review);
 };
 
 module.exports = {
   getReviews,
+  getReviewById,
 };
