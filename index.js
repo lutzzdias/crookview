6a2b38d3ee057fe7af195420954573fc538c4f99
lutzzdias <thiagolutzdias@hotmail.com>
2022-11-06 20:23:35 -0400

    Make index prettier

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express')
+const express = require("express");
 
-const review_controller = require('./controllers/review_controller')
-const item_controller = require("./controllers/item_controller")
+const review_controller = require("./controllers/review_controller");
+const item_controller = require("./controllers/item_controller");
 
-const app = express()
-const PORT = 3060
+const app = express();
+const PORT = 3060;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api', (req, res) => {
-    res.send('Home page')
-})
+app.get("/api", (req, res) => {
+  res.send("Home page");
+});
 
-app.use('/api/review', review_controller)
+app.use("/api/review", review_controller);
 app.use("/api/item", item_controller);
 
-app.listen(PORT, () => console.log(`api running on: http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`api running on: http://localhost:${PORT}`));
