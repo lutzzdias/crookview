440074032cbc54bb96b107151f81278ad5434af6
lutzzdias <thiagolutzdias@hotmail.com>
2022-11-13 17:35:30 -0400

    Fix handle error, make code prettier and refactor user service

--- services/item_service.js
+++ services/item_service.js
@@ -1,74 +1,72 @@
-const { Item } = require("../models/item");
-
+const { Item } = require("../models");
 
 const getItems = async (req, res) => {
-    try{
+  try {
     const item = await Item.findAll();
     res.status(200).json(item);
-    }catch(error){
-        handleError(error);
-    };
-    
-} 
-
-const getItemById = async(req, res)=>{
-    const id = req.params.id;
-
-    try{
-        const item = await Item.findByPk(id);
-
-        if(item == null) return res.status(404).send("Item not found.");
-
-        res.status(200).json(item);
-    }catch(error){
-        handleError(error);
-    }
+  } catch (error) {
+    handleError(error, res);
+  }
 };
 
-const createItem = async (req, res) =>{
-    const { title, description, date, image, type} = req.body;
-    try {
-        const newItem = await Item.create({
-            title: title,
-            description: description,
-            date: date,
-            image: image,
-            type: type,
-        });
-        res.status(201).json(newItem);
-    } catch (error){
-        handleError (error);
-    }
+const getItemById = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const item = await Item.findByPk(id);
+
+    if (item == null) return res.status(404).send("Item not found.");
+
+    res.status(200).json(item);
+  } catch (error) {
+    handleError(error, res);
+  }
 };
 
-const updateItem = async (req, res) =>{
-    const id = req.params.id;
-    const { title, description, date, image, type} = req.body;
-
-    try{
-        const [_, [result]] = await Item.update(
-            {
-                title: title,
-                description: description,
-                date: date,
-                image: image,
-                type: type,
-            },
-            { where: {id: id}, returning: true}
-        );
-        res.status(200).json(result);
-    } catch (error){
-        handleError(error);
-    }
+const createItem = async (req, res) => {
+  const { title, description, date, image, type } = req.body;
+  try {
+    const newItem = await Item.create({
+      title: title,
+      description: description,
+      date: date,
+      image: image,
+      type: type,
+    });
+    res.status(201).json(newItem);
+  } catch (error) {
+    handleError(error, res);
+  }
 };
 
-const handleError = (error) => {
-    console.log(error);
-    return res.status(500).json({ error: error.message });
-  };
+const updateItem = async (req, res) => {
+  const id = req.params.id;
+  const { title, description, date, image, type } = req.body;
+
+  try {
+    const [_, [result]] = await Item.update(
+      {
+        title: title,
+        description: description,
+        date: date,
+        image: image,
+        type: type,
+      },
+      { where: { id: id }, returning: true }
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    handleError(error, res);
+  }
+};
+
+const handleError = (error, res) => {
+  console.log(error);
+  return res.status(500).json({ error: error.message });
+};
 module.exports = {
-    getItems,
-    getItemById,
-    createItem,
-    updateItem,
+  getItems,
+  getItemById,
+  createItem,
+  updateItem,
 };
--- services/user_service.js
+++ services/user_service.js
@@ -4,17 +4,17 @@ const bcrypt = require("bcryptjs");
 const createUser = async (req, res) => {
   const { username, password, email, image } = req.body;
 
-  bcrypt.genSalt(10, (erro, salt) => {
-    bcrypt.hash(password, salt, (erro, salt) => {
-      if (erro) {
-        return res.status(500).send("Error during user save");
+  bcrypt.genSalt(10, (error, salt) => {
+    bcrypt.hash(password, salt, (error, salt) => {
+      if (error) {
+        handleError(error, res);
       }
     });
   });
   try {
     const emailExists = checkEmailExistence(email);
     if (emailExists == true)
-      return res.status(400).send("Email is already register.");
+      return res.status(400).send("Email is already registered.");
 
     const newUser = await User.create({
       username: username,
@@ -22,22 +22,20 @@ const createUser = async (req, res) => {
       email: email,
       image: image,
     });
-    res.status(200).send("User created with sucess");
+
+    res.status(201).json(newUser);
   } catch (error) {
-    handleError(error);
+    handleError(error, res);
   }
 };
 
 const checkEmailExistence = async (email) => {
   const user = await User.findOne(email);
-  if (user == null) {
-    return false;
-  } else {
-    return true;
-  }
+  if (user == null) return false;
+  else return true;
 };
 
-const handleError = (error) => {
+const handleError = (error, res) => {
   console.log(error);
   return res.status(500).json({ error: error.message });
 };
