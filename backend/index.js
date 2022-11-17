c6215366d0bff321f72a4e4c09468349a51f83ab
lutzzdias <thiagolutzdias@hotmail.com>
2022-11-17 15:58:27 -0400

    Add config in order to load css

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // EJS config
+app.use(express.static("../frontend/public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
