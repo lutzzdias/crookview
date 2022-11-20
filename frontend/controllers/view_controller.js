ce466cb29b67260071c0ef4ed2ffc4cfdc8cd5ef
lutzzdias <thiagolutzdias@hotmail.com>
2022-11-20 16:18:02 -0400

    Integrate series and books view and backend

--- frontend/controllers/view_controller.js
+++ frontend/controllers/view_controller.js
@@ -4,6 +4,8 @@ const router = Router();
 
 router.get("/", async (req, res) => await getHomeView(req, res));
 router.get("/movies", async (req, res) => await getMoviesView(req, res));
+router.get("/series", async (req, res) => await getSeriesView(req, res));
+router.get("/books", async (req, res) => await getBooksView(req, res));
 
 const getHomeView = async (req, res) => {
   info = await getHomeInfo();
@@ -14,6 +16,18 @@ const getMoviesView = async (req, res) => {
   const response = await axios.get("http://localhost:3060/api/item/movies");
   const movies = response.data;
   res.render("movies", { movies: movies });
+};
+
+const getSeriesView = async (req, res) => {
+  const response = await axios.get("http://localhost:3060/api/item/series");
+  const series = response.data;
+  res.render("series", { series: series });
+};
+
+const getBooksView = async (req, res) => {
+  const response = await axios.get("http://localhost:3060/api/item/books");
+  const books = response.data;
+  res.render("books", { books: books });
 };
 
 const getHomeInfo = async () => {
