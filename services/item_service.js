09a0f953d5f4df6f3c317dcf951d6bdc511f8dd5
lutzzdias <thiagolutzdias@hotmail.com>
2022-11-20 16:41:12 -0400

    Add nested include in getItemById

--- services/item_service.js
+++ services/item_service.js
@@ -1,5 +1,4 @@
-const { Item } = require("../models");
-const { Review } = require("../models");
+const { Item, Review, User } = require("../models");
 
 const getItems = async (req, res) => {
   try {
@@ -66,7 +65,9 @@ const getItemById = async (req, res) => {
   const id = req.params.id;
 
   try {
-    const item = await Item.findByPk(id, { include: Review });
+    const item = await Item.findByPk(id, {
+      include: [{ model: Review, include: [User] }],
+    });
 
     if (item) return res.status(200).json(item);
     else return res.status(404).send("Item not found.");
