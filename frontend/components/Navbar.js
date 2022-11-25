103c8e900949d3769be5d2e2d3dc1ed5dcc751e4
lutzzdias <thiagolutzdias@hotmail.com>
2022-11-25 14:47:49 -0400

    Implement items screen and navigability through navbar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from '../styles/Navbar.module.css';
 
 // TODO: Change navBar item color onClick
@@ -5,25 +6,31 @@ export default function Navbar() {
   return (
     <div>
       <nav className={styles.navbar}>
-        <a href="#" className="logo">
+        <a href="/" className="logo">
           Logo
         </a>
         <div className={styles.navLinks}>
           <ul className={styles.navMenu}>
             <li className={styles.active}>
-              <a href="">Home</a>
+              <Link href="/">Home</Link>
             </li>
             <li>
-              <a href="">Movies</a>
+              <Link href={{ pathname: 'items', query: { type: 'movies' } }}>
+                Movies
+              </Link>
             </li>
             <li>
-              <a href="">Series</a>
+              <Link href={{ pathname: 'items', query: { type: 'series' } }}>
+                Series
+              </Link>
             </li>
             <li>
-              <a href="">Books</a>
+              <Link href={{ pathname: 'items', query: { type: 'books' } }}>
+                Books
+              </Link>
             </li>
             <li>
-              <a href="">Login</a>
+              <Link href="login">Login</Link>
             </li>
           </ul>
         </div>
