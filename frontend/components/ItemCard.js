84a9b491fc4492f9590310f77e0827b8e8ec7995
lutzzdias <thiagolutzdias@hotmail.com>
2022-11-25 11:21:52 -0400

    Make itemCard clickable

--- frontend/components/ItemCard.js
+++ frontend/components/ItemCard.js
@@ -1,19 +1,20 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/ItemCard.module.css';
 import itemImage from '../images/shrek.jpg';
 
 export default function ItemCard(props) {
   return (
-    <div className={styles.item}>
+    <Link href={`/item/${props.item.id}`} className={styles.item}>
       <div className={styles.containerImg}>
         <Image src={itemImage} className={styles.itemImg} />
       </div>
 
       <div className={styles.itemInfo}>
         <span className={styles.itemType}>{props.item.type}</span>
-        <a className={styles.itemName}>{props.item.name}</a>
+        <span className={styles.itemName}>{props.item.name}</span>
         <span className={styles.itemDescription}>{props.item.description}</span>
       </div>
-    </div>
+    </Link>
   );
 }
